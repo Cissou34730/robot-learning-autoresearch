d839a589a743a692a5c63a357b56baf6dcae56c0
Cissou34730 <cyril.beurier@yahoo.fr>
2026-09-09 11:00:33 +0200

    experiment 10 scientific recipe

--- robot_learning/scenario/observations.py
+++ robot_learning/scenario/observations.py
@@ -34,11 +34,13 @@ def reach_observation(data) -> np.ndarray:
     elbow_folded = -elbow_open
     shoulder_folded = shoulder_for_elbow(elbow_folded)
     end_effector = data.site("end_effector").xpos.copy()
+    target_delta = end_effector - data.mocap_pos[0]
     return np.concatenate(
         [
             data.qpos,
             data.qvel,
-            end_effector - data.mocap_pos[0],
+            target_delta[:2],
+            [np.linalg.norm(target_delta)],
             [
                 wrap_to_pi(shoulder_open - float(data.qpos[0])),
                 wrap_to_pi(elbow_open - float(data.qpos[1])),
