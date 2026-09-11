4b9cd3737a044ba460a6bc1b91a51e3d5a0fc672
Cissou34730 <cyril.beurier@yahoo.fr>
2026-09-11 05:48:04 +0200

    experiment 15 code reverted to its scientific parent

--- robot_learning/scenario/policy_io.py
+++ robot_learning/scenario/policy_io.py
@@ -10,39 +10,21 @@ from robot_learning.policy_runtime import PolicyIO
 from robot_learning.scenario.observations import reach_observation
 
 ACTION_SMOOTHING_FACTOR = 0.5
-HARD_SECTOR_SMOOTHING_FACTOR = 0.75
-HARD_SECTOR_MIN_ANGLE = np.deg2rad(-160.0)
-HARD_SECTOR_MAX_ANGLE = np.deg2rad(-115.0)
 
 
 def make_policy_io():
     previous_action = np.zeros(2, dtype=np.float64)
-    latest_target_angle = None
-
-    def observe(data):
-        nonlocal latest_target_angle
-        latest_target_angle = float(
-            np.arctan2(data.mocap_pos[0][1], data.mocap_pos[0][0])
-        )
-        return reach_observation(data)
 
     def physical_action(action):
-        smoothing_factor = ACTION_SMOOTHING_FACTOR
-        if latest_target_angle is not None and (
-            HARD_SECTOR_MIN_ANGLE <= latest_target_angle < HARD_SECTOR_MAX_ANGLE
-        ):
-            smoothing_factor = HARD_SECTOR_SMOOTHING_FACTOR
         command = np.asarray(action, dtype=np.float64)
         smoothed = (
-            smoothing_factor * command
-            + (1.0 - smoothing_factor) * previous_action
+            ACTION_SMOOTHING_FACTOR * command
+            + (1.0 - ACTION_SMOOTHING_FACTOR) * previous_action
         )
         previous_action[:] = smoothed
         return smoothed
 
     def reset():
-        nonlocal latest_target_angle
         previous_action.fill(0.0)
-        latest_target_angle = None
 
-    return PolicyIO(observe=observe, action=physical_action, reset=reset)
+    return PolicyIO(observe=reach_observation, action=physical_action, reset=reset)
--- tests/scenario/test_policy_io.py
+++ tests/scenario/test_policy_io.py
@@ -3,17 +3,6 @@ import numpy as np
 from robot_learning.scenario.policy_io import make_policy_io
 
 
-class _FakeData:
-    def __init__(self, angle):
-        self.mocap_pos = np.array([[np.cos(angle), np.sin(angle), 0.0]])
-        self.qpos = np.zeros(2)
-        self.qvel = np.zeros(2)
-
-    def site(self, name):
-        assert name == "end_effector"
-        return type("Site", (), {"xpos": np.zeros(3)})()
-
-
 def test_action_mapping_smooths_command_changes():
     policy_io = make_policy_io()
 
@@ -27,19 +16,5 @@ def test_action_mapping_reset_clears_previous_command():
 
     assert policy_io.reset is not None
     policy_io.reset()
-
-    np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.5, -0.5])
-
-
-def test_action_mapping_reduces_lag_in_residual_target_sector():
-    policy_io = make_policy_io()
-    policy_io.observe(_FakeData(np.deg2rad(-130.0)))
-
-    np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.75, -0.75])
-
-
-def test_action_mapping_keeps_baseline_smoothing_outside_residual_sector():
-    policy_io = make_policy_io()
-    policy_io.observe(_FakeData(np.deg2rad(30.0)))
 
     np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.5, -0.5])
