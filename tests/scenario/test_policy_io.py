47b2a46d3089c1afd0c34cb0d36a160f7a6ab05d
Cissou34730 <cyril.beurier@yahoo.fr>
2026-09-11 02:25:12 +0200

    experiment 8 scientific recipe

--- tests/scenario/test_policy_io.py
+++ tests/scenario/test_policy_io.py
@@ -6,8 +6,10 @@ from robot_learning.scenario.policy_io import make_policy_io
 def test_action_mapping_smooths_command_changes():
     policy_io = make_policy_io()
 
-    np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.5, -0.5])
     np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.75, -0.75])
+    np.testing.assert_allclose(
+        policy_io.action(np.array([1.0, -1.0])), [0.9375, -0.9375]
+    )
 
 
 def test_action_mapping_reset_clears_previous_command():
@@ -17,4 +19,4 @@ def test_action_mapping_reset_clears_previous_command():
     assert policy_io.reset is not None
     policy_io.reset()
 
-    np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.5, -0.5])
+    np.testing.assert_allclose(policy_io.action(np.array([1.0, -1.0])), [0.75, -0.75])
