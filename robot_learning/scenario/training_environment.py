646fe8bcf1dbe92297a108f05762d9966c8c6b9e
Cissou34730 <cyril.beurier@yahoo.fr>
2026-09-18 09:55:26 +0200

    experiment 6 scientific recipe

--- robot_learning/scenario/training_environment.py
+++ robot_learning/scenario/training_environment.py
@@ -13,7 +13,10 @@ import numpy as np
 from robot_learning.scenario.environment import TwoJointArmReachEnv
 
 TRAINING_TARGET_RADIUS_RANGE = (0.06, 0.20)
-FOCUSED_TARGET_ANGLE_RANGE = (np.deg2rad(110.0), np.deg2rad(170.0))
+FOCUSED_TARGET_ANGLE_RANGES = (
+    (np.deg2rad(-170.0), np.deg2rad(-110.0)),
+    (np.deg2rad(110.0), np.deg2rad(170.0)),
+)
 FOCUSED_TARGET_ANGLE_PROBABILITY = 0.25
 
 
@@ -22,7 +25,10 @@ class FocusedAngleReachEnv(TwoJointArmReachEnv):
 
     def _sample_target_position(self) -> None:
         if self.np_random.random() < FOCUSED_TARGET_ANGLE_PROBABILITY:
-            angle = float(self.np_random.uniform(*FOCUSED_TARGET_ANGLE_RANGE))
+            focused_range = FOCUSED_TARGET_ANGLE_RANGES[
+                int(self.np_random.integers(len(FOCUSED_TARGET_ANGLE_RANGES)))
+            ]
+            angle = float(self.np_random.uniform(*focused_range))
         else:
             angle = float(self.np_random.uniform(-np.pi, np.pi))
 
