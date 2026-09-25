43f282904c196d417635781196ef2d1053c7f47d
Cissou34730 <cyril.beurier@yahoo.fr>
2026-09-25 17:12:12 +0200

    camp: experiment 3 code retained for best_known

--- robot_learning/scenario/observations.py
+++ robot_learning/scenario/observations.py
@@ -8,8 +8,7 @@ import numpy as np
 
 from robot_learning.robots.two_joint_arm import FOREARM_LENGTH, UPPER_ARM_LENGTH
 
-OBSERVATION_SIZE = 13
-JOINT_LIMIT_RADIANS = float(np.deg2rad(170.0))
+OBSERVATION_SIZE = 11
 
 
 def reach_observation(data) -> np.ndarray:
@@ -34,13 +33,6 @@ def reach_observation(data) -> np.ndarray:
     shoulder_open = shoulder_for_elbow(elbow_open)
     elbow_folded = -elbow_open
     shoulder_folded = shoulder_for_elbow(elbow_folded)
-
-    def branch_limit_margin(shoulder: float, elbow: float) -> float:
-        return min(
-            (JOINT_LIMIT_RADIANS - abs(shoulder)) / JOINT_LIMIT_RADIANS,
-            (JOINT_LIMIT_RADIANS - abs(elbow)) / JOINT_LIMIT_RADIANS,
-        )
-
     end_effector = data.site("end_effector").xpos.copy()
     return np.concatenate(
         [
@@ -52,10 +44,6 @@ def reach_observation(data) -> np.ndarray:
                 wrap_to_pi(elbow_open - float(data.qpos[1])),
                 wrap_to_pi(shoulder_folded - float(data.qpos[0])),
                 wrap_to_pi(elbow_folded - float(data.qpos[1])),
-            ],
-            [
-                branch_limit_margin(shoulder_open, elbow_open),
-                branch_limit_margin(shoulder_folded, elbow_folded),
             ],
         ]
     ).astype(np.float32)
